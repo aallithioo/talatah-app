5213f010f609ff13a6632f9d85a1c079cfc2fdce
Thio Alli <connect.thioalli@gmail.com>
2022-01-13 10:01:18 +0700

    add : view/contact/

---
+++ lib/src/view/contact/screen/contact_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactScreen extends StatefulWidget {
+  const ContactScreen({Key? key}) : super(key: key);
+
+  @override
+  _ContactScreenState createState() => _ContactScreenState();
+}
+
+class _ContactScreenState extends State<ContactScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

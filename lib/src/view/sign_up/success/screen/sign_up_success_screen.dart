779d582b6bbe873f3b317aa2d324266686c3ba2f
Thio Alli <connect.thioalli@gmail.com>
2022-01-13 09:44:06 +0700

    feat : stateful - sign_up_success_screen.dart

--- lib/src/view/sign_up/success/screen/sign_up_success_screen.dart
+++ lib/src/view/sign_up/success/screen/sign_up_success_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SignUpAuthScreen extends StatefulWidget {
-  const SignUpAuthScreen({Key? key}) : super(key: key);
+class SignUpSuccessScreen extends StatefulWidget {
+  const SignUpSuccessScreen({Key? key}) : super(key: key);
 
   @override
-  _SignUpAuthScreenState createState() => _SignUpAuthScreenState();
+  _SignUpSuccessScreenState createState() => _SignUpSuccessScreenState();
 }
 
-class _SignUpAuthScreenState extends State<SignUpAuthScreen> {
+class _SignUpSuccessScreenState extends State<SignUpSuccessScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();

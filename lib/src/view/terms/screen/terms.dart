1a0f7e3100a539ba4262b304d8d0b7866376f69a
Thio Alli <connect.thioalli@gmail.com>
2022-01-13 09:57:23 +0700

    refactor : profile_screen.dart

---
+++ lib/src/view/terms/screen/terms.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TermsScreen extends StatefulWidget {
+  const TermsScreen({Key? key}) : super(key: key);
+
+  @override
+  _TermsScreenState createState() => _TermsScreenState();
+}
+
+class _TermsScreenState extends State<TermsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

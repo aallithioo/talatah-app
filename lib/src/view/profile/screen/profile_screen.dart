efdcc8de23d5a8be3586be2e3d9e69975a4b208f
Thio Alli <connect.thioalli@gmail.com>
2022-01-13 09:53:47 +0700

    initial

---
+++ lib/src/view/profile/screen/profile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileSScreen extends StatefulWidget {
+  const ProfileSScreen({Key? key}) : super(key: key);
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileSScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/src/view/profile/setting/screen/profile_setting.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileSettingScreen extends StatefulWidget {
+  const ProfileSettingScreen({Key? key}) : super(key: key);
+
+  @override
+  _ProfileSettingScreenState createState() => _ProfileSettingScreenState();
+}
+
+class _ProfileSettingScreenState extends State<ProfileSettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

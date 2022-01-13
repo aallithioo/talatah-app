7e1ecd916f853017062f2f81f205a00452289bab
Thio Alli <connect.thioalli@gmail.com>
2022-01-13 09:41:51 +0700

    feat : stateless - forgot_password_auth.dart

---
+++ lib/src/view/sign_in/forgot/auth/screen/forgot_password_auth_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordAuthScreen extends StatefulWidget {
+  const ForgotPasswordAuthScreen({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPasswordAuthScreenState createState() =>
+      _ForgotPasswordAuthScreenState();
+}
+
+class _ForgotPasswordAuthScreenState extends State<ForgotPasswordAuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

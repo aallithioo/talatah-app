c2943bfc81809a9550f5ab47b737336b405048b5
Thio Alli <connect.thioalli@gmail.com>
2022-01-28 22:01:28 +0700

    feat : body - intro_screen.dart

--- lib/src/view/intro/intro_screen.dart
+++ lib/src/view/intro/intro_screen.dart
@@ -15,6 +15,8 @@ class IntroScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: IntroBody(),
+    );
   }
 }

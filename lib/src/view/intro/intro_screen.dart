665b8530e6e5e9b1cf2ebade5cd024eceb5c9d8e
Thio Alli <connect.thioalli@gmail.com>
2022-01-28 21:57:41 +0700

    Major Update

--- lib/src/view/intro/intro_screen.dart
+++ lib/src/view/intro/intro_screen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../../app/themes/color.dart';
+import '../../app/themes/size.dart';
+import '../../app/themes/textalign.dart';
+import '../../app/themes/theme.dart';
+import '../../app/widgets/custom_border.dart';
+import '../../app/widgets/custom_padding.dart';
+
 part './widgets/intro_body.dart';
+part './widgets/intro_content.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({Key? key}) : super(key: key);

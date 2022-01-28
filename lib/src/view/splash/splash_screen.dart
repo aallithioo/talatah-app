29cf44a42e01a2d9c6ba34ca72f1f008638279cf
Thio Alli <connect.thioalli@gmail.com>
2022-01-28 21:20:47 +0700

    refactor : splash_screen.dart

--- lib/src/view/splash/splash_screen.dart
+++ lib/src/view/splash/splash_screen.dart
@@ -11,7 +11,7 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SplashBody(),
     );
   }

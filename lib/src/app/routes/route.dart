b57cd9de2a6a6f0c723e0ad74ca0987186947122
Thio Alli <connect.thioalli@gmail.com>
2022-01-28 19:42:58 +0700

    feat : route - route.dart

--- lib/src/app/routes/route.dart
+++ lib/src/app/routes/route.dart
@@ -2,49 +2,30 @@ class Routes {
   // Splash Screen
   static const splash = '/';
 
-  // Warn Screen
-  static const warn = '/warn';
-
   // Intro Screen
-  static const intro = '/intro';
-
-  // Sign Up Screen
-  static const signUp = '/signup';
-  static const signUpAuth = '/signup/auth';
-  static const signUpSuccess = '/signup/auth/success';
-
-  // Sign In Screen
-  static const signIn = '/signin';
-  static const signInSuccess = '/signin/success';
-  static const forgot = '/signin/forgot';
-  static const forgotAuth = '/signin/forgot/auth';
-  static const reset = '/signin/reset';
-  static const resetSuccess = '/signin/reset/success';
-
-  // Sign Up Screen
-  static const signOut = '/signout';
-
-  // Terms & Conditions Screen
-  static const terms = '/terms';
-
-  // Privacy Policy Screen
-  static const privacy = '/privacy';
+  static const intro = '/app';
 
   // Home Screen
   static const home = '/app/home';
 
-  // Favorites Screen
-  static const myLearning = '/app/my_learning';
+  // Setting Screen
+  static const setting = '/app/setting';
 
-  // Messages Screen
-  static const myQuiz = '/app/my_quiz';
+  // Release Notes Screen
+  static const releaseNotes = '/app/setting/release-notes';
 
-  // Profile Screen
-  static const profile = '/app/profile';
-  static const profileDetails = '/app/profile/details';
-  static const notifications = '/app/profile/notifications';
-  static const setting = '/app/profile/settings';
-  static const support = '/app/profile/support';
-  static const help = '/app/profile/help';
-  static const about = '/app/profile/about';
+  // About Screen
+  static const about = '/app/setting/about';
+
+  // Support Screen
+  static const support = '/app/setting/support';
+
+  // Terms Screen
+  static const terms = '/app/setting/terms';
+
+  // Privacy Screen
+  static const privacy = '/app/setting/privacy';
+
+  // Report Bug Screen
+  static const reportBug = '/app/setting/report-bug';
 }

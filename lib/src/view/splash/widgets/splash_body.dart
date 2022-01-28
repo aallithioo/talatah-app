25972cbd5f6e9edf6c3b423c769f08a77f03cbb2
Thio Alli <connect.thioalli@gmail.com>
2022-01-28 21:17:27 +0700

    refactor : splash/

--- lib/src/view/splash/widgets/splash_body.dart
+++ lib/src/view/splash/widgets/splash_body.dart
@@ -1,1 +1,20 @@
 part of '../splash_screen.dart';
+
+class SplashBody extends StatelessWidget {
+  const SplashBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: kPaddingSymetricHorizontalLarge,
+        child: Center(
+          child: Text(
+            'Splash Screen',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+        ),
+      ),
+    );
+  }
+}

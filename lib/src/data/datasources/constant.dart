033e30f81311a3990f6560bcc5b0b6e3b8bd33dc
Thio Alli <connect.thioalli@gmail.com>
2022-01-29 19:21:42 +0700

    add : constant.dart

---
+++ lib/src/data/datasources/constant.dart
@@ -0,0 +1,8 @@
+class Init {
+  static const String taltahDbName = 'talatah.db';
+  static const String talatahTableName = 'talatah';
+  static const String talatahId = 'id';
+  static const String talatahVendor = 'vendor';
+  static const String talatahEmail = 'emailk';
+  static const String talatahPassword = 'password';
+}

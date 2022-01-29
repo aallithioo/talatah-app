5a94959b1e0854f538f5def7947b0732976567c0
Thio Alli <connect.thioalli@gmail.com>
2022-01-29 08:51:05 +0700

    feat : list item data - info/

--- lib/src/app/widgets/custom_appbar.dart
+++ lib/src/app/widgets/custom_appbar.dart
@@ -1,3 +1,5 @@
+import 'package:aallithioo/src/app/themes/color.dart';
+import 'package:aallithioo/src/app/themes/size.dart';
 import 'package:aallithioo/src/app/themes/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -5,44 +7,16 @@ import '../themes/fontweight.dart';
 
 AppBar kAppBar(String title, [Icon? firstIcon, Icon? secondIcon]) {
   return AppBar(
-    title: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          title,
-          style: kThioAlli.textTheme.headline6!.copyWith(
-            color: kThioAlli.textTheme.headline6!.color,
-            fontWeight: kFontWeightMedium,
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            // IconButton(
-            //   icon: firstIcon ??
-            //       Icon(
-            //         Icons.notifications_rounded,
-            //         color: kWhiteColorShade500,
-            //       ),
-            //   color: kWhiteColorShade500,
-            //   onPressed: () {},
-            // ),
-            // IconButton(
-            //   icon: secondIcon ??
-            //       Icon(
-            //         Icons.shopping_bag_rounded,
-            //         color: kWhiteColorShade500,
-            //       ),
-            //   color: kWhiteColorShade500,
-            //   onPressed: () {},
-            // ),
-          ],
-        ),
-      ],
+    title: Text(
+      title,
+      style: kThioAlli.textTheme.headline6!.copyWith(
+        color: kAccentColor,
+        fontSize: kSizeMedium,
+        fontWeight: kFontWeightMedium,
+      ),
     ),
     backgroundColor: Colors.transparent,
+    centerTitle: true,
     foregroundColor: kThioAlli.textTheme.headline1!.color,
     shadowColor: Colors.transparent,
     elevation: 0.0,

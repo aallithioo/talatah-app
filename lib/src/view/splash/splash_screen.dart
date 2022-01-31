c37f4c589b76eb2881a1aa173efd3928fd62c55e
Thio Alli <connect.thioalli@gmail.com>
2022-01-31 22:16:42 +0700

    refactor : route - splash_screen.dart

--- lib/src/view/splash/splash_screen.dart
+++ lib/src/view/splash/splash_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../../app/routes/route.dart';
 import '../../app/themes/theme.dart';
-
 import '../../app/widgets/custom_padding.dart';
 
 part './widgets/splash_body.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, Routes.intro);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {

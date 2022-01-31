241d970277e5a9971b78c35067676558e722cc6c
Thio Alli <connect.thioalli@gmail.com>
2022-01-31 22:21:44 +0700

    add : setting/

---
+++ lib/src/view/setting/setting_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../../app/enums.dart';
+import '../../app/themes/color.dart';
+import '../../app/widgets/custom_bottom_nav_bar.dart';
+
+part './widgets/setting_body.dart';
+part './widgets/setting_content.dart';
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: SettingBody(),
+      bottomNavigationBar:
+          const CustomBottomNavBar(selectedMenu: MenuState.setting),
+    );
+  }
+}
---
+++ lib/src/view/setting/widgets/setting_body.dart
@@ -0,0 +1,10 @@
+part of '../setting_screen.dart';
+
+class SettingBody extends StatelessWidget {
+  const SettingBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

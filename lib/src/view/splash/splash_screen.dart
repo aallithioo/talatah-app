427673b6bc6c4641ea5535e0dde89088af6ee651
Thio Alli <connect.thioalli@gmail.com>
2022-02-16 13:06:00 +0700

    refactor : import - splash_screen.dart

--- lib/src/view/splash/splash_screen.dart
+++ lib/src/view/splash/splash_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:talatah/src/app/themes/font_weight.dart';
-import 'package:talatah/src/app/themes/size.dart';
-import 'package:talatah/src/app/widgets/padding.dart';
 
 import '../../app/routes/route.dart';
+import '../../app/themes/font_weight.dart';
+import '../../app/themes/size.dart';
 import '../../app/themes/color.dart';
 import '../../app/themes/theme.dart';
+import '../../app/widgets/padding.dart';
 
 part './widgets/splash_body.dart';
 part './widgets/splash_content.dart';

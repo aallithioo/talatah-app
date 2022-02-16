b698f016c8e498b6ecc9e893e125cfc325c9da28
Thio Alli <connect.thioalli@gmail.com>
2022-02-16 09:47:33 +0700

    refactor : font_weight.dart

---
+++ lib/src/app/themes/font_weight.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SetFontWeight {
+  static const thin = FontWeight.w100;
+  static const extraLight = FontWeight.w200;
+  static const light = FontWeight.w300;
+  static const regular = FontWeight.w400;
+  static const medium = FontWeight.w500;
+  static const semibold = FontWeight.w600;
+  static const bold = FontWeight.w700;
+  static const heavy = FontWeight.w800;
+  static const black = FontWeight.w900;
+}
